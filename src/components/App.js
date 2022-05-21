290649dc0a823314503bfdfd36553f406c558ba3
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-21 19:05:37 +0530

    Subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,44 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-        <div className='main'>
-            <div className='tabs'>
-              <div className='tab'>Movies</div>
-              <div className='tab'>Favourites</div>
-            </div>  
+class App extends React.Component{
+  componentDidMount(){
+    const {store}=this.props;
+    // subscribe will be called after the dispatch action
+    store.subscribe(()=>{
+      console.log('UPDATED');
+      this.forceUpdate();
+    })
+    //make an api call to get the movies
+    //we dispatch an action
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    });
+    console.log('STATE',this.props.store.getState());
+  }
+  render(){
+    const movies= this.props.store.getState();
+    console.log('RENDER');
+    return (
+      <div className="App">
+        <Navbar />
+          <div className='main'>
+              <div className='tabs'>
+                <div className='tab'>Movies</div>
+                <div className='tab'>Favourites</div>
+              </div>  
 
-            <div className="list">
-              {/* we can iterate over the movies using the data.map,then we will pass the movie to the moviecard via props*/}
-              {data.map((movie,index) =>(
-                  <MovieCard movie={movie} key={`movies-${index}`} />
-              ))}
-            </div>
-        </div>
-    </div>
-  );
+              <div className="list">
+                {/* we can iterate over the movies using the data.map,then we will pass the movie to the moviecard via props*/}
+                {movies.map((movie,index) =>(
+                    <MovieCard movie={movie} key={`movies-${index}`} />
+                ))}
+              </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
