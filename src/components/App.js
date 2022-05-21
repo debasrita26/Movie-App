331a228cb590546abeb118230e29b9618dc03116
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-21 23:50:16 +0530

    adding movies to the favourite tab

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,21 @@ class App extends React.Component{
     store.dispatch(addMovies(data));
     console.log('STATE',this.props.store.getState());
   }
+
+  //check if the movie is in the favourites array or not
+  isMovieFavourite=(movie)=>{
+    const {favourites}= this.props.store.getState();
+    const index=favourites.indexOf(movie);
+    if(index !==-1 ){
+      //found the movie
+      return true;
+    }
+    return false;
+
+  }
   render(){
     const {list}= this.props.store.getState();
-    console.log('RENDER');
+    console.log('RENDER',this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
@@ -32,7 +44,10 @@ class App extends React.Component{
               <div className="list">
                 {/* we can iterate over the movies using the data.map,then we will pass the movie to the moviecard via props*/}
                 {list.map((movie,index) =>(
-                    <MovieCard movie={movie} key={`movies-${index}`} />
+                    <MovieCard movie={movie} 
+                      key={`movies-${index}`} 
+                      dispatch={this.props.store.dispatch}
+                      isFavourite={this.isMovieFavourite(movie)} />
                 ))}
               </div>
           </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import {ADD_MOVIES} from '../actions';
+import {ADD_MOVIES,ADD_FAVOURITE} from '../actions';
 
 const initialMoviesState={
     list:[],
@@ -7,12 +7,28 @@ const initialMoviesState={
 
 //currect state will never be undefined so we will set it as empty,and next argument action will be passed
 export default function movies(state = initialMoviesState, action){
-    if(action.type==='ADD_MOVIES'){
-        return {
-            ...state,
-            list:action.movies
-        }
+    // if(action.type==='ADD_MOVIES'){
+    //     return {
+    //         ...state,
+    //         list:action.movies
+    //     }
+    // }
+    // return state;
+
+    switch(action.type){
+        case ADD_MOVIES:
+            return{
+                ...state,
+                list:action.movies
+            }
+        case ADD_FAVOURITE:
+            return{
+                ...state,
+                // adding the movie at the first index and then rest of the movies in the array
+                favourites: [action.movie, ...state.favourites]
+            }
+        default:
+            return state;
     }
-    return state;
 }
 
