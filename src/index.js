432aa04020e67c6e046ddd6d867fba6bdd3458a3
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-21 18:13:06 +0530

    created the configure store and also added redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
+
+const store = configureStore({reducer: movies});
+console.log('store',store);
+console.log('STORE',store.getState);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
