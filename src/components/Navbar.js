be6285f833da0afee93ca1c305aeab3b3769c8b5
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-22 15:42:30 +0530

    Add to movies functionality added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {handleAddToList } from '../actions';
+import {addMovieToList } from '../actions';
 
 class Navbar extends React.Component{
     handleAddToMovies = (movie) => {
-        this.props.dispatch(handleAddToList(movie));
-      };
+        this.props.dispatch(addMovieToList(movie));
+    }
     render(){
         return (
             <div className="nav">
                 <div className='search-container'>
                     <input />
                     <button id='search-btn'>Search</button>
-                    {<div className="movie-info">
-                        <span>{movie.Title}</span>
-                        <button onClick={() => this.handleAddToMovies(movie)}>
-                            Add To Movies
-                        </button>
+                    {
+                        <div className='movie-info'>
+                            <button onClick={() => this.handleAddToMovies(this.props.search.result)}>Add To Movies</button>
                         </div>
                     }
                 </div>
