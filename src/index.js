ffd83e40baecbfd325967f93d2636856b49ea45c
debasrita.banerjee <debasrita.banerjee262gmail.com>
2022-05-24 22:36:42 +0530

    Context API functionality added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
@@ -35,7 +35,20 @@ const logger=({dispatch,getState})=>(next)=>(action)=>{
 // }
 
 const store = configureStore({reducer:rootReducer},applyMiddleware(logger,thunk));
-console.log('store',store);
+console.log('store',store.getState());
+
+export const StoreContext = createContext();
+console.log('StoreContext',StoreContext);
+
+class Provider extends React.Component{
+  render(){
+    const {store}=this.props;
+    // pass the store around every descendent and pass the value prop
+    return <StoreContext.Provider value={store}>
+     {this.props.children} 
+    </StoreContext.Provider>
+  }
+}
 // console.log('BEFORE STATE',store.getState());
 
 // //dispatch function is used to send actions,this takes an action object as argument
@@ -47,8 +60,10 @@ console.log('store',store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     {/* pass store as a prop to the app */}
     <App store={store} />
   </React.StrictMode>
+  </Provider>
 );
